ea88a8d98da3f0360f79c4ea58e287540427a6e9
yrien30 <goran.yri@vendanor.com>
2024-07-04 21:39:05 +0200

    Initial commit

---
+++ Health/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using AndroidX.Health.Connect.Client;
+using AndroidX.Health.Connect.Client.Aggregate;
+using AndroidX.Health.Connect.Client.Request;
+using AndroidX.Health.Connect.Client.Records;
+using Health.Platforms.Android.Callbacks;
+using AndroidX.Health.Connect.Client.Time;
+using AndroidX.Health.Connect.Client.Records.Metadata;
+using Java.Time;
+using AndroidX.Activity.Result.Contract;
+using AndroidX.Activity.Result;
+using AndroidX.Health.Connect.Client.Permission;
+using Android.Runtime;
+using Kotlin.Jvm.Internal;
+using Java.Util;
+using Health.Platforms.Android.Permissions;
+using AndroidX.Health.Connect.Client.Units;
+
+
+namespace Health
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        static ActivityResultContract activityResultContract = PermissionController.CreateRequestPermissionResultContract();
+        private ActivityResultLauncher launcher = ((AndroidX.Activity.ComponentActivity)Platform.CurrentActivity).RegisterForActivityResult(activityResultContract, new ActivityResultCallback());
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private class ActivityResultCallback : Java.Lang.Object, IActivityResultCallback
+        {
+            public void OnActivityResult(Java.Lang.Object? result) // Make the parameter nullable
+            {
+                if (result is ISet PermissionGranted)
+                {
+                    ;
+                }
+
+            }
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            var zonedDateTime = ZonedDateTime.Now();
+            string startTimeString = zonedDateTime.MinusDays(0).MinusHours(zonedDateTime.Hour).MinusMinutes(zonedDateTime.Minute).MinusSeconds(zonedDateTime.Second).ToString().Substring(0, 19) + "Z";
+            Instant startTime = Instant.Parse(startTimeString);
+ 
+
+            Instant endTime = Instant.Parse(ZonedDateTime.Now().ToString().Substring(0, 19) + "Z");
+            ZoneOffset startZoneOffset = ZoneOffset.OfHours(2);
+            ZoneOffset endZoneOffset = ZoneOffset.OfHours(1);
+            Metadata metadata = new Metadata();
+            StepsRecord stepsRecord = new StepsRecord(startTime,startZoneOffset, endTime, endZoneOffset, 1, metadata);
+            DistanceRecord distanceRecord = new DistanceRecord(startTime, startZoneOffset, endTime, endZoneOffset, Length.InvokeMeters(11), metadata);
+
+            var javaSet = new HashSet();
+            javaSet.Add(HealthPermission.GetReadPermission(Reflection.GetOrCreateKotlinClass(stepsRecord.Class)));
+            javaSet.Add(HealthPermission.GetReadPermission(Reflection.GetOrCreateKotlinClass(distanceRecord.Class)));
+
+
+            if (OperatingSystem.IsAndroidVersionAtLeast(26))
+            {
+
+                //int availabilityStatus = HealthConnectClient.GetSdkStatus(Platform.CurrentActivity);
+                
+
+                ICollection<AggregateMetric> metrics = new List<AggregateMetric> { StepsRecord.CountTotal, DistanceRecord.DistanceTotal };
+
+                ICollection<DataOrigin> dataOrginFilter = new List<DataOrigin>();
+
+                AggregateGroupByDurationRequest request = new AggregateGroupByDurationRequest(metrics, TimeRangeFilter.After(startTime), Duration.OfDays(1), dataOrginFilter);
+
+                
+                try
+                {
+                    var healthConnectClient = new KotlinCallback(HealthConnectClient.GetOrCreate(Android.App.Application.Context));
+
+                    List<string> GrantedPermissions = await healthConnectClient.GetGrantedPermissions();
+                    //Check if missing permissons 
+                    ISet PermissionResult = await PermissionHandler.Request(javaSet);
+
+                    var Result = await healthConnectClient.AggregateGroupByDuration(request);
+                    int? StepCountTotal = null;
+                    Java.Lang.Object? DistanceTotal;
+
+                    foreach (AggregationResultGroupedByDuration item in Result)
+                    {
+
+                        if (item.Result.Contains(StepsRecord.CountTotal))
+                        {
+                            StepCountTotal = (int)item.Result.Get(StepsRecord.CountTotal).JavaCast<Java.Lang.Number>();
+                        }
+
+                        if (item.Result.Contains(DistanceRecord.DistanceTotal))
+                        {
+                            DistanceTotal = item.Result.Get(DistanceRecord.DistanceTotal);
+                        }
+                    }
+                    CounterBtn.Text = StepCountTotal.ToString();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+    }
+
+}
---
+++ Health/Platforms/Android/Permissions/PermissionHandler.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Util;
+using AndroidX.Core.App;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JObject = Java.Lang.Object;
+
+namespace Health.Platforms.Android.Permissions
+{
+    internal class PermissionHandler
+    {
+        public static async Task<ISet> Request(Java.Lang.Object permissions, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+
+                if (Platform.CurrentActivity is not MainActivity activity)
+                    return null;
+
+                var whenCompletedSource = new TaskCompletionSource<JObject?>();
+                _ = Task.Delay(MainActivity.MaxPermissionRequestDuration, cancellationToken)
+                    .ContinueWith(_ => whenCompletedSource.TrySetResult(null), TaskScheduler.Default);
+
+
+                new AlertDialog.Builder(activity)
+                    .SetTitle("Health connect permission isn't granted")
+                    .SetMessage("Do you want to allow permissions?")
+                    .SetNegativeButton("Decline", (_, _) => whenCompletedSource.TrySetResult(null))
+                    .SetPositiveButton("Allow", (_, _) => RequestPermission())
+                    .Show();
+
+
+                JObject? result = await whenCompletedSource.Task.ConfigureAwait(false);
+                return (ISet)result;
+
+                void RequestPermission()
+                => activity.RequestPermission(permissions, whenCompletedSource);
+            }
+            catch (Exception ex)
+            {
+                return null;
+                // Handle exceptions as needed, e.g., logging or user notification
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
